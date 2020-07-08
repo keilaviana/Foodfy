cd0467b938b8b6ca08dd796834deb1fe3aee4259
Keila Viana <keilavg72@gmail.com>
2020-07-08 15:20:31 -0300

    add primeira rota

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const nunjucks = require("nunjucks")
 
 const server = express() 
 
-server.get("view engine", "html")
+server.set("view engine", "html")
 nunjucks.configure("views", {
     express: server
 })
 
 server.get("/", function(req, res){
-    return res.send("enviando msg")
+    return res.render("about")
 })
 
 server.listen(5500, function(){
