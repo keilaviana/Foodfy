27386876f116cba7dc968119a57a03925d2aaa9f
Keila Viana <keilavg72@gmail.com>
2020-07-09 22:10:08 -0300

    add lógica para esconder e mostrar informações das receitas

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll('.card')
+const status = document.querySelectorAll('.status')
 
 for(let card of cards){
     card.addEventListener("click", function(){
@@ -6,3 +7,33 @@ for(let card of cards){
         window.location.href=`/recipes/${indexId}`
     })
 }
+
+for(let botao of status){
+    botao.addEventListener("click", function(){
+        if(querySelector('.lista').style.display == "block"){
+            querySelector('.lista').style.display == "none"
+            modo.innerHTML = "MOSTRAR"
+        }else{
+            querySelector('.lista').style.display == "block"
+            modo.innerHTML = "ESCONDER"
+        }
+    })
+}
+
+const buttons = document.querySelectorAll('.status')
+const infos = document.querySelectorAll('.lista')
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        if (infos[i].style.display == 'none') {
+            infos[i].style.display = 'block'
+            buttons[i].textContent = "ESCONDER"
+        } else {
+            infos[i].style.display = 'none'
+            buttons[i].textContent = "MOSTRAR"
+        }
+    })
+}
+
+
+
